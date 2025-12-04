11fcf7fad39f8317383dd9380ad9866a0762fa18
ChenDan89 <1241341732@qq.com>
2025-12-04 08:56:20 +0000

    Deploying to gh-pages from @ ChenDan89/ChenDan89.github.io@a07fa96a2bdacc31e9e103d211b03336412c694e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,14 +18,14 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum vitae for Dan Chen (陈聃).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
